e8111e6d515e787e0ef940f407deef93ad2752b7
chikhidev <chikhi.dev@gmail.com>
2023-11-19 21:46:55 +0100

    optimizing..

--- out.utils/run.setup.bash
+++ out.utils/run.setup.bash
@@ -1,4 +1,9 @@
-alias seth="python out.utils/header_setter.py"
 run() {
     cc ft_printf.c utils/*.c tests/"$1".test.c && ./a.out
+}
+
+push(){
+    git add .
+    git commit -m $1
+    git push origin master
 }
