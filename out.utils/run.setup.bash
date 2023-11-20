e6e028960736fd82c056424a105a9a97e43e3a3e
chikhidev <chikhi.dev@gmail.com>
2023-11-20 12:00:11 +0100

    fixing tests

--- out.utils/run.setup.bash
+++ out.utils/run.setup.bash
@@ -2,7 +2,7 @@ test() {
     if [[ $2 -eq 1 ]]; then
         gcc -Wall -Wextra -Werror ft_printf.c utils/*.c tests/${1}.test.c -o exec/${1}.output && ./exec/${1}.output
     else
-        gcc ft_printf.c utils/*.c tests/${1}.test.c -o exec/${1}.output && ./exec/${1}.output | cat -e
+        gcc ft_printf.c utils/*.c tests/${1}.test.c tests/test.c -o exec/${1}.output && ./exec/${1}.output | cat -e
     fi
 }
 
